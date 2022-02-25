11791b26ace2a2d41eb263248f9663567f2b74fe
Eifion Bedford <eifion.bedford@gmail.com>
2022-02-25 09:30:10 +0000

    Now displays current balance.

--- LibFunds/Balances.cs
+++ LibFunds/Balances.cs
@@ -16,13 +16,12 @@ public class Balances
     {
         using IDbConnection conn = new SqliteConnection(_dbConnection);
         var parameters = new { DateTime.Today };
-        //         var balances = conn.Query<int>(@"
-        // SELECT Amount * CAST(julianday(@Today) - julianday(StartDate) + 1 AS INTEGER) / CAST(julianday(EndDate) - julianday(StartDate) + 1 AS INTEGER)
-        // FROM Incomings WHERE EndDate > @Today
-        // UNION ALL SELECT SUM(Amount) FROM Incomings WHERE EndDate <= @Today
-        // UNION ALL SELECT -1 * SUM(Amount) FROM Outgoings
-        // UNION ALL SELECT SUM(OpeningBalance) FROM Funds", parameters);
-        var balances = conn.Query<int>("SELECT SUM(opening_balance) FROM Funds");
+        var balances = conn.Query<int>(@"
+         SELECT Amount * CAST(julianday(@Today) - julianday(start_date) + 1 AS INTEGER) / CAST(julianday(end_date) - julianday(start_date) + 1 AS INTEGER)
+         FROM Incomings WHERE end_date > @Today
+         UNION ALL SELECT SUM(Amount) FROM Incomings WHERE end_date <= @Today
+         UNION ALL SELECT -1 * SUM(amount) FROM Outgoings WHERE date <= @Today
+         UNION ALL SELECT SUM(opening_balance) FROM Funds", parameters);
         return balances.Sum() / 100.0m;
     }
 }
